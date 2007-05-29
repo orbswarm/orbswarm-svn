efdceae97d2e2d34ae17bc7522fb84440a1d7125
swarm <info@orbswarm.com>
2007-05-29 21:59:35 +0000

    Add shared directories for things common to orb and mothership
    git-svn-id: http://svn.lee.org/swarm/trunk@24 47a5675e-1c2e-0410-904b-cc838fe3ce0b

---
+++ shared/include/packet_type.h
@@ -0,0 +1,19 @@
+/*
+   packet types for the RF link between orbs and mothership
+
+   These from SPU meeting 28 May 2007
+*/
+
+enum ELinkPacketType {
+
+  eLinkRTCM         = 0x00,   // Binary data
+  eLinkMotorControl,
+  eLinkArt,
+  eLinkNMEA,
+  eLinkNavigation,            // Including trajectory and such
+  eLinkCCS,                   // Command Control and Status
+  eLinkConsole,
+  eLinkCodeDownload,
+  eLinkLastPacketType
+};
+
