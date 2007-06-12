db8e41e0b3f2f5ea4cdbb28f6d6c16175bdd0dbf
swarm <info@orbswarm.com>
2007-06-12 03:25:03 +0000

    Added COM1... COM5 #defines
    git-svn-id: http://svn.lee.org/swarm/trunk@34 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- spu/include/swarmserial.h
+++ spu/include/swarmserial.h
@@ -13,6 +13,14 @@
 #ifndef SWARM_SERIAL_H 
 #define SWARM_SERIAL_H 
 
+/* defines for TS-7260 serial ports */
+#define COM1 "/dev/ttyAM0"
+#define COM2 "/dev/ttyAM1"
+#define COM3 "/dev/ttyTS0"
+#define COM4 "/dev/ttyTS1"
+#define COM5 "/dev/ttyTS2"
+
+
 int initSerialPort(const char* port, int baud);
 
 void readCharsFromSerialPort(int port_fd, char* buff, 
