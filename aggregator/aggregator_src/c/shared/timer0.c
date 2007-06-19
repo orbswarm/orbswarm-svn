a2db26c0ff59dedd657e3d5c2355a7c026732a75
swarm <info@orbswarm.com>
2007-06-19 06:02:38 +0000

    makefile changes to build atmega640
    git-svn-id: http://svn.lee.org/swarm/trunk@58 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- aggregator/aggregator_src/c/shared/timer0.c
+++ aggregator/aggregator_src/c/shared/timer0.c
@@ -9,7 +9,7 @@ ISR(SIG_OVERFLOW0)
   if(++timecount == m_unitsOf1ms)
     {
       //disable timer we are done
-      TIMSK = TIMSK & (~(1<<TOIE0));
+      TIMSK0 = TIMSK0 & (~(1<<TOIE0));
     }
 }
 
@@ -20,8 +20,8 @@ int loopTimer0(uint16_t units1ms)
   timecount=0;
   m_unitsOf1ms=units1ms;
   TCNT0=6;
-  TCCR0 = TCCR0 | ((1<< CS01)|(1<<CS00))  ;
-  TIMSK=TIMSK | (1<<TOIE0);
+  TCCR0A = TCCR0A | ((1<< CS01)|(1<<CS00))  ;
+  TIMSK0= TIMSK0 | (1<<TOIE0);
   sei();
   while(timecount<m_unitsOf1ms)
     ;
