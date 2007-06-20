21f7a4eb84a8d8423a74e4dba012ff99c362e5f4
swarm <info@orbswarm.com>
2007-06-20 22:48:29 +0000

    Initial Checkin-- General spu utilities for the swarm project.  First routine is for toggling the spus onboard led's.
    git-svn-id: http://svn.lee.org/swarm/trunk@68 47a5675e-1c2e-0410-904b-cc838fe3ce0b

---
+++ spu/include/swarmspuutils.h
@@ -0,0 +1,18 @@
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/mman.h>
+#include<stdio.h>
+#include<fcntl.h>
+#include<string.h>
+#include "swarmdefines.h"
+
+/* This is a collection of general utility methods for working with the spu */
+#ifndef SWARM_SPU_UTILS_H 
+#define SWARM_SPU_UTILS_H 
+/************************
+ * Sets the indicated spu led state as defined in swarmdefines.h
+ */
+int toggleSpuLed(const unsigned int ledState);  
+//void set_led(int led);
+
+#endif
