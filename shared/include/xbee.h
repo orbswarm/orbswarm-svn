04cba39fc3c781be851596742f1b66509249aa8b
swarm <info@orbswarm.com>
2007-06-20 21:15:50 +0000

    xbee packet types
    git-svn-id: http://svn.lee.org/swarm/trunk@67 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- shared/include/xbee.h
+++ shared/include/xbee.h
@@ -1,11 +1,24 @@
 /*
   XBee message definition and common code to be used 
-  by both SPU and aggreagtor
+  by both SPU and aggreagtor.
 */
 
 /*
-  XBee message format for 64 addresses
+  XBee message formats for 64 bit addresses
 */
+
+#define MAX_SWARM_MSG_LENGTH 97
+
+/*
+  For now the assumption is that there will bbe one swarm message
+  per xbee packet. May change in the future
+*/
+struct SWARM_MSG{
+  unsigned char swarm_msg_type[1];
+  unsigned char swarm_msg_length[2];
+  unsigned char swarm_msg_payload[MAX_SWARM_MSG_LENGTH];
+};
+
 struct XBEE_RX_PACKET {
   unsigned char start_byte; //value=0x7e
   unsigned char packet_length[2];
@@ -16,7 +29,6 @@ struct XBEE_RX_PACKET {
   unsigned char payload[100];
 } ;
 
-struct XBEE_RX_PACKET parse_packet(unsigned char* raw_packet);
 
 struct XBEE_TX_PACKET {
   unsigned char start_byte; //value=0x7e
@@ -28,4 +40,5 @@ struct XBEE_TX_PACKET {
   unsigned char payload[100];
 }  ;
 
-unsigned char* build_packet(struct XBEE_TX_PACKET tx_packet); 
+struct XBEE_RX_PACKET parse_packet(unsigned char* raw_packet);
+void build_packet(struct XBEE_TX_PACKET tx_packet, unsigned char* raw_packet); 
