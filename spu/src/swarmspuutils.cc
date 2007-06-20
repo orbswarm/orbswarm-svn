12240a3fcc2ae7b147cff50ad1e4b6c05fe2fb1c
swarm <info@orbswarm.com>
2007-06-20 22:52:31 +0000

    Initial Checkin-- General spu utilities for the swarm project.  First routine is for toggling the spus onboard led's.
    git-svn-id: http://svn.lee.org/swarm/trunk@70 47a5675e-1c2e-0410-904b-cc838fe3ce0b

---
+++ spu/src/swarmspuutils.cc
@@ -0,0 +1,60 @@
+/**************************
+ * Implementation file for swarmspuutils.h
+ */
+
+#include "../include/swarmspuutils.h"
+
+int toggleSpuLed(const unsigned int ledState)
+{  
+   volatile unsigned int *PEDR, *PEDDR;
+   unsigned char *start;
+   int fd = open("/dev/mem", O_RDWR|O_SYNC);
+
+   start =(unsigned char*) mmap(0, getpagesize(), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0x80840000);
+   PEDR = (unsigned int *)(start + 0x20);     // port e data
+   PEDDR = (unsigned int *)(start + 0x24);    // port e direction register
+
+   *PEDDR = 0xff;                             // all output (just 2 bits)
+
+     if(ledState == SPU_LED_RED_ON) 
+     {
+       *PEDR = 0xf0; 
+     }
+     else if(ledState == SPU_LED_GREEN_ON)
+     {
+       *PEDR = 0x0f; 
+     }
+     else if(ledState == SPU_LED_BOTH_ON)
+     {
+       *PEDR = 0xff; 
+     }
+     else if(ledState == SPU_LED_BOTH_OFF)
+     {
+       *PEDR = 0x00; 
+     }
+     else if(ledState == SPU_LED_RED_OFF)
+     {
+       *PEDR = *PEDR & 0x0f; 
+     }
+     else if(ledState == SPU_LED_GREEN_OFF)
+     {
+       *PEDR = *PEDR & 0xf0; 
+     }
+     else
+     {
+       fprintf(stderr,"\nUNKNOWN LED STATE");
+     }
+   
+   // blink 5 times, sleep 1 second so it's visible
+   /*
+   for (i = 0; i < 5; i++) {
+      *PEDR = 0xff;
+      sleep(1);
+      *PEDR = 0x00;
+      sleep(1);
+   }
+   */
+   close(fd);
+   return 0;
+}
+
---
+++ spu/src/testspuutils.cc
@@ -0,0 +1,24 @@
+#include "../include/swarmspuutils.h"
+
+int main(int argc, char *argv[]) 
+{
+  fprintf(stderr,"\nSTART swarmspuutils toggleSpuLed TESTS");
+
+  fprintf(stderr,"\nSwitching on Red spu led for 5 seconds");
+  toggleSpuLed(SPU_LED_RED_ON);  
+  sleep(5);
+  fprintf(stderr,"\nSwitching Red spu led off");
+  toggleSpuLed(SPU_LED_RED_OFF);  
+  fprintf(stderr,"\nSwitching on Green spu led for 5 seconds");
+  toggleSpuLed(SPU_LED_GREEN_ON);  
+  sleep(5);
+  fprintf(stderr,"\nSwitching Green spu led off");
+  toggleSpuLed(SPU_LED_RED_OFF);  
+  fprintf(stderr,"\nSwitching on Green and Red spu led's for 5 seconds");
+  toggleSpuLed(SPU_LED_BOTH_ON);  
+  sleep(5);
+  fprintf(stderr,"\nSwitching both spu leds off");
+  toggleSpuLed(SPU_LED_BOTH_OFF);  
+  fprintf(stderr,"\nEND swarmspuutils toggleSpuLed TESTS");
+ return 0;
+}
