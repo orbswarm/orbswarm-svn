aee61f722dead3b86484e8b085f73b3a69224905
swarm <info@orbswarm.com>
2007-06-20 22:49:15 +0000

    Added defines for led toggle routine
    git-svn-id: http://svn.lee.org/swarm/trunk@69 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- spu/include/swarmdefines.h
+++ spu/include/swarmdefines.h
@@ -3,3 +3,11 @@
 #define MAX_BUFF_SZ 512 //Serial port read/write buffer size
 #define SWARM_SUCCESS 0
 #define SWARM_SERIAL_WRITE_ERR 10 //failed to write data to serial port 
+
+#define SPU_LED_RED_ON 40  
+#define SPU_LED_GREEN_ON 41 
+#define SPU_LED_BOTH_ON 42 
+#define SPU_LED_BOTH_OFF 43  
+#define SPU_LED_RED_OFF 44   
+#define SPU_LED_GREEN_OFF 45 
+
