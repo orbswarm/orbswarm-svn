0a341bb4f972976bd8da2c1c806529771a6c8e49
swarm <info@orbswarm.com>
2007-06-26 05:12:54 +0000

    implementations for FSM XBee parser, aggregator queue etc.
    git-svn-id: http://svn.lee.org/swarm/trunk@87 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- aggregator/build/makelib.mk
+++ aggregator/build/makelib.mk
@@ -1,12 +1,12 @@
 #Build shared lib first
 LIB_SRC= $(SVN_TRUNK)/aggregator/aggregator_src/c/shared
 LIB_BIN = $(SVN_TRUNK)/aggregator/binaries/shared
-#build_lib: $(LIB_BIN)/timer0.o $(LIB_BIN)/uart.o 
-build_lib: $(LIB_BIN)/timer0.o
+build_lib: $(LIB_BIN)/timer0.o $(LIB_BIN)/uart.o $(LIB_BIN)/xbee.o
+#build_lib: $(LIB_BIN)/timer0.o
 		
 $(LIB_BIN)/%.o : $(LIB_SRC)/%.c	
 	@echo  '-----Building libraries--------'
-	avr-gcc 	-c -I. -g -Os 	-funsigned-char \
+	avr-gcc 	-c -I. -I $(GLOBAL_SHARED_INCLUDE) -g -Os 	-funsigned-char \
 	-funsigned-bitfields -fpack-struct 	\
 	-fshort-enums -Wall -Wstrict-prototypes -DF_CPU=$(F_CPU) \
  	-Wa,-adhlns=$*.lst  -mmcu=$(MCU) -std=gnu99 $< -o $@
