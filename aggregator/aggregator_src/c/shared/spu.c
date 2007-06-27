2fc20ea3c242209cc0a88e3b680ce5214b7c5a72
swarm <info@orbswarm.com>
2007-06-27 08:18:08 +0000

    it's getting late
    git-svn-id: http://svn.lee.org/swarm/trunk@94 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- aggregator/aggregator_src/c/shared/spu.c
+++ aggregator/aggregator_src/c/shared/spu.c
@@ -70,13 +70,13 @@ void handleSpuSerialRx(unsigned char c, int isError)
     if(spu_state_byte_num < spu_exp_payload_len)
       {
 	spu_recv_msg.swarm_msg_payload[spu_state_byte_num++]=c;
-	break;
       }
-    if(((spu_state_byte_num+1)== spu_exp_payload_len)
+    if((spu_state_byte_num+1)== spu_exp_payload_len)
       {
 	//handle msh here
 	handleSpuSwarmMsg(spu_recv_msg);
       }
+      break;
   default :
     //init
     initSpuVars();
