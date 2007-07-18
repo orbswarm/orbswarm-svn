131d70a87f056771c51793b859e40048857a7cdd
swarm <info@orbswarm.com>
2007-07-18 06:52:47 +0000

    added methods to convert from nmea gps data to decimal long lat
    git-svn-id: http://svn.lee.org/swarm/trunk@117 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- spu/include/swarmdefines.h
+++ spu/include/swarmdefines.h
@@ -1,8 +1,16 @@
 /******Swarm Defines include file *********/
+#ifndef SWARM_DEFINES_H
+#define SWARM_DEFINES_H 1
 
 #define MAX_BUFF_SZ 512 //Serial port read/write buffer size
 #define SWARM_SUCCESS 0
 #define SWARM_SERIAL_WRITE_ERR 10 //failed to write data to serial port 
+#define SWARM_OUT_OF_MEMORY_ERROR 11 //failed to malloc some memory 
+#define SWARM_INVALID_GPS_SENTANCE 100 
+#define SWARM_NMEA_GPS_SENTANCE_TYPE_GPGGA "GPGGA"
+#define SWARM_NMEA_GPS_DATA_DELIM ","
+#define MAX_GPS_SENTANCE_SZ 1024 
+
 
 #define SPU_LED_RED_ON 40  
 #define SPU_LED_GREEN_ON 41 
@@ -20,3 +28,22 @@
 #define LOG_FILE_BASE_NAME "swarm_log_"
 #define DEFAULT_LOG_PATH "./logs"
 
+/*************************STRUCTURES************************************/
+struct	swarmGpsData
+{
+  char gpsSentanceType[32]; 
+  char gpsSentance[MAX_GPS_SENTANCE_SZ];
+  char nmea_utctime[64];
+  long double nmea_latddmm;
+  long double nmea_londdmm;
+  char nmea_latsector;
+  char nmea_lonsector;
+  long double latdd;
+  long double londd;
+  long double utcTime;
+  long double metFromMshipX;
+  long double metFromMshipY;
+  long double utcTimeMship;
+};
+
+#endif
--- spu/include/swarmspuutils.h
+++ spu/include/swarmspuutils.h
@@ -8,6 +8,7 @@
 #include <time.h>
 #include <errno.h>
 #include "swarmdefines.h"
+#include <stdlib.h>
 
 /* This is a collection of general utility methods for working with the spu */
 #ifndef SWARM_SPU_UTILS_H 
@@ -21,5 +22,14 @@ int resetOrbMCU(void);
 
 //pass NULL for log dir to use default path defined in swarmdefines.h
 int spulog(char* msg, int msgSize, char* logdir);
+
+//Parses an NMEA data sentance into the seperate components needed for conversion
+//Returns SWARM_SUCCESS on a successful parse and  SWARM_INVALID_GPS_SENTANCE if 
+//the input string was garbage or an invalid type.  The parser only accepts NMEA
+//sentances of type GPGGA 
+int parseGPSSentance(swarmGpsData * gpsdata);
+
+//converts the raw NMEA gps lat long data into decimal lat long data
+int convertNMEAGpsLatLonDataToDecLatLon(swarmGpsData * gpsdata);
 
 #endif
