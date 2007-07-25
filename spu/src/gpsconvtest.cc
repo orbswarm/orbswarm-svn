9bbab91566724197e9d170f92cb970b88524b95f
swarm <info@orbswarm.com>
2007-07-25 17:19:48 +0000

    Added code for lat/lon to UTM conversion
    git-svn-id: http://svn.lee.org/swarm/trunk@130 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- spu/src/gpsconvtest.cc
+++ spu/src/gpsconvtest.cc
@@ -8,11 +8,13 @@ int main(int argc, char *argv[])
 {
    FILE           *in_file;    /* input file */ 
    FILE           *out_file;    /* output file */ 
+   FILE           *utm_out_file;    /* output file */ 
    char lineBuff[1024];
    swarmGpsData * gpsdata = NULL;
    int status = SWARM_SUCCESS;
    in_file = fopen(argv[1], "r");
    out_file = fopen(argv[2], "w");
+   utm_out_file = fopen(argv[3], "w");
 
    fprintf(stderr,"\n Got infile name as :%s",argv[1]); 
    if (in_file == NULL) {
@@ -32,8 +34,12 @@ int main(int argc, char *argv[])
         if(status == SWARM_SUCCESS)
         {
           //fprintf(stderr,"\n Decimal lat:%Lf lon:%Lf utctime:%s \n",gpsdata->latdd,gpsdata->londd,gpsdata->nmea_utctime);
-          fprintf(out_file,"%s,%.12Lf,%.13Lf\n",gpsdata->nmea_utctime,gpsdata->londd,gpsdata->latdd);
+          fprintf(out_file,"%s,%.12f,%.13f\n",gpsdata->nmea_utctime,gpsdata->londd,gpsdata->latdd);
+          
+          decimalLatLongtoUTM(WGS84_EQUATORIAL_RADIUS_METERS, WGS84_ECCENTRICITY_SQUARED, gpsdata);
+          fprintf(utm_out_file,"Northing:%f,Easting:%f,UTMZone:%s\n",gpsdata->UTMNorthing,gpsdata->UTMEasting,gpsdata->UTMZone);
         }
+        
      }
    
      free(gpsdata);
--- spu/src/swarmspuutils.cc
+++ spu/src/swarmspuutils.cc
@@ -199,7 +199,7 @@ int parseGPSSentance(swarmGpsData * gpsdata)
             break;
           case 2:
             //nmea format latddmm
-            sscanf(paramptr,"%Lf",&(gpsdata->nmea_latddmm));
+            sscanf(paramptr,"%lf",&(gpsdata->nmea_latddmm));
             //fprintf(stderr, "\n latddmm: %s",paramptr);
             break;
           case 3:
@@ -209,7 +209,7 @@ int parseGPSSentance(swarmGpsData * gpsdata)
             break;
           case 4:
             //nmea format londdmm
-            sscanf(paramptr,"%Lf",&(gpsdata->nmea_londdmm));
+            sscanf(paramptr,"%lf",&(gpsdata->nmea_londdmm));
             //fprintf(stderr, "\n londdmm: %s",paramptr);
             break;
           case 5:
@@ -234,10 +234,10 @@ int parseGPSSentance(swarmGpsData * gpsdata)
 
 int convertNMEAGpsLatLonDataToDecLatLon(swarmGpsData * gpsdata)
 {
-   long double latdd = 0;
-   long double londd = 0;
-   long double latmm = 0;
-   long double lonmm = 0;
+   double latdd = 0;
+   double londd = 0;
+   double latmm = 0;
+   double lonmm = 0;
 
    //get the degress part
    latdd = (int)(gpsdata->nmea_latddmm / 100);
@@ -261,5 +261,95 @@ int convertNMEAGpsLatLonDataToDecLatLon(swarmGpsData * gpsdata)
    gpsdata->londd = londd;
 
    return SWARM_SUCCESS;  
+}
+
+char UTMLetterDesignator(double Lat)
+{
+        //This routine determines the correct UTM letter designator for the given latitude
+        //returns 'Z' if latitude is outside the UTM limits of 84N to 80S
+	char LetterDesignator;
+
+	if((84 >= Lat) && (Lat >= 72)) LetterDesignator = 'X';
+	else if((72 > Lat) && (Lat >= 64)) LetterDesignator = 'W';
+	else if((64 > Lat) && (Lat >= 56)) LetterDesignator = 'V';
+	else if((56 > Lat) && (Lat >= 48)) LetterDesignator = 'U';
+	else if((48 > Lat) && (Lat >= 40)) LetterDesignator = 'T';
+	else if((40 > Lat) && (Lat >= 32)) LetterDesignator = 'S';
+	else if((32 > Lat) && (Lat >= 24)) LetterDesignator = 'R';
+	else if((24 > Lat) && (Lat >= 16)) LetterDesignator = 'Q';
+	else if((16 > Lat) && (Lat >= 8)) LetterDesignator = 'P';
+	else if(( 8 > Lat) && (Lat >= 0)) LetterDesignator = 'N';
+	else if(( 0 > Lat) && (Lat >= -8)) LetterDesignator = 'M';
+	else if((-8> Lat) && (Lat >= -16)) LetterDesignator = 'L';
+	else if((-16 > Lat) && (Lat >= -24)) LetterDesignator = 'K';
+	else if((-24 > Lat) && (Lat >= -32)) LetterDesignator = 'J';
+	else if((-32 > Lat) && (Lat >= -40)) LetterDesignator = 'H';
+	else if((-40 > Lat) && (Lat >= -48)) LetterDesignator = 'G';
+	else if((-48 > Lat) && (Lat >= -56)) LetterDesignator = 'F';
+	else if((-56 > Lat) && (Lat >= -64)) LetterDesignator = 'E';
+	else if((-64 > Lat) && (Lat >= -72)) LetterDesignator = 'D';
+	else if((-72 > Lat) && (Lat >= -80)) LetterDesignator = 'C';
+	else LetterDesignator = 'Z'; //error flag indicating that the Latitude is outside the UTM limits
+
+	return LetterDesignator;
+}
+
+void decimalLatLongtoUTM(const double ref_equ_radius, const double ref_ecc_squared, swarmGpsData * gpsdata)
+{
+        double Lat = gpsdata->latdd;
+        double Long = gpsdata->londd;
+
+	double a = ref_equ_radius;
+	double eccSquared = ref_ecc_squared;
+	double k0 = 0.9996;
+
+	double LongOrigin;
+	double eccPrimeSquared;
+	double N, T, C, A, M;
+	
+//Make sure the longitude is between -180.00 .. 179.9
+	double LongTemp = (Long+180)-int((Long+180)/360)*360-180; // -180.00 .. 179.9;
+
+	double LatRad = Lat*deg2rad;
+	double LongRad = LongTemp*deg2rad;
+	double LongOriginRad;
+	int    ZoneNumber;
+
+	ZoneNumber = int((LongTemp + 180)/6) + 1;
+  
+	if( Lat >= 56.0 && Lat < 64.0 && LongTemp >= 3.0 && LongTemp < 12.0 )
+		ZoneNumber = 32;
+
+  // Special zones for Svalbard
+	if( Lat >= 72.0 && Lat < 84.0 ) 
+	{
+	  if(      LongTemp >= 0.0  && LongTemp <  9.0 ) ZoneNumber = 31;
+	  else if( LongTemp >= 9.0  && LongTemp < 21.0 ) ZoneNumber = 33;
+	  else if( LongTemp >= 21.0 && LongTemp < 33.0 ) ZoneNumber = 35;
+	  else if( LongTemp >= 33.0 && LongTemp < 42.0 ) ZoneNumber = 37;
+	 }
+	LongOrigin = (ZoneNumber - 1)*6 - 180 + 3;  //+3 puts origin in middle of zone
+	LongOriginRad = LongOrigin * deg2rad;
+
+	//compute the UTM Zone from the latitude and longitude
+	sprintf(gpsdata->UTMZone, "%d%c", ZoneNumber, UTMLetterDesignator(Lat));
+
+	eccPrimeSquared = (eccSquared)/(1-eccSquared);
+
+	N = a/sqrt(1-eccSquared*sin(LatRad)*sin(LatRad));
+	T = tan(LatRad)*tan(LatRad);
+	C = eccPrimeSquared*cos(LatRad)*cos(LatRad);
+	A = cos(LatRad)*(LongRad-LongOriginRad);
+
+	M = a*((1 - eccSquared/4 - 3*eccSquared*eccSquared/64	- 5*eccSquared*eccSquared*eccSquared/256)*LatRad - (3*eccSquared/8	+ 3*eccSquared*eccSquared/32	+ 45*eccSquared*eccSquared*eccSquared/1024)*sin(2*LatRad) + (15*eccSquared*eccSquared/256 + 45*eccSquared*eccSquared*eccSquared/1024)*sin(4*LatRad) - (35*eccSquared*eccSquared*eccSquared/3072)*sin(6*LatRad));
+	
+	gpsdata->UTMEasting = (double)(k0*N*(A+(1-T+C)*A*A*A/6
+					+ (5-18*T+T*T+72*C-58*eccPrimeSquared)*A*A*A*A*A/120)
+					+ 500000.0);
+
+	gpsdata->UTMNorthing = (double)(k0*(M+N*tan(LatRad)*(A*A/2+(5-T+9*C+4*C*C)*A*A*A*A/24
+				 + (61-58*T+T*T+600*C-330*eccPrimeSquared)*A*A*A*A*A*A/720)));
+	if(Lat < 0)
+	  gpsdata->UTMNorthing += 10000000.0; //10000000 meter offset for southern hemisphere
 }
 
