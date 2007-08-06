23ea42024eb3afdc8b527eb928e36b5fd8e89789
swarm <info@orbswarm.com>
2007-08-06 01:22:44 +0000

    added much code for testing
    git-svn-id: http://svn.lee.org/swarm/trunk@197 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- spu/src/mainloop.cc
+++ spu/src/mainloop.cc
@@ -8,9 +8,6 @@
 #include "../include/swarmdefines.h"
 #include "../include/swarmspuutils.h"
 
-#define GPS_START_DELIM "\n{"
-#define GPS_STOP_DELIM "}\n"
-
 #define VERBOSE 1
 
 int main(int argc, char *argv[]) 
@@ -19,25 +16,14 @@ int main(int argc, char *argv[])
   int com1=0; /* File descriptor for the port */
   int com2=0; /* File descriptor for the port */
   int com3=0, com4=0, com5=0; 	/* ditto */
-  int n;
   int tenHzticks = 0;
   char buffer[MAX_BUFF_SZ + 1];
-  int gps_byte_index = 0;
-  bool end_gps_sentance = false;
-
-  char *gps_start_str = GPS_START_DELIM; // string to indicate start of GPS data
-  char *gps_stop_str = GPS_STOP_DELIM; // string to indicate end of GPS data
   int bytes2 = 0;
-  int total_gps_bytes = 0;
-  char *gpsQueryString = "$Ag*$";
-  
-  int             max_fd;
-  fd_set          input;
   struct timeval tv;
-  //  com1 = initSerialPort("/dev/ttyAM0", 38400);
-  com2 = initSerialPort("/dev/ttyAM1", 38400);
-  com4 = initSerialPort(COM4, 38400);
-  com5 = initSerialPort(COM5, 38400);
+  int status = SWARM_SUCCESS;
+  int             max_fd;
+  fd_set          fdset;
+  int n =0; 
 
   // need to find max fd for select()
   max_fd = (com2 > com1 ? com2 : com1) + 1;
@@ -45,175 +31,210 @@ int main(int argc, char *argv[])
   max_fd = (com4 > max_fd ? com4 : max_fd) + 1;
   max_fd = (com5 > max_fd ? com5 : max_fd) + 1;
 
+  swarmGpsData motherShipLoc; 
+  swarmGpsData currentOrbLoc; 
+
+  com2 = initSerialPort("/dev/ttyAM1", 38400); //Aggregator
+  com3 = initSerialPort(COM3, 38400); //Lighting and Effects
+  com5 = initSerialPort(COM5, 38400); //Motor Controller
 
   // main loop
   while(1)
   {
-    /* Initialize the input set */
-     FD_ZERO(&input);
-     FD_SET(com2, &input); // COM2 talks to the aggregator
-     FD_SET(com4, &input); // COM4 talks to the GPS now
-                           // (eventually it will be sound & LED: output only)
-     FD_SET(com5, &input); // COM5 talks to the motor controller
-
+     ///////////////////////////// MAIN LOOP //////////////////////////
      // set a 10 hz timeout for the main loop
      tv.tv_sec = 0; 
      tv.tv_usec = 100000; // 100 ms or 10 hz
 
-     /* Do the select */
-     //n = select(max_fd, &input, NULL, NULL,&tv);
+     FD_ZERO(&fdset);
 
-     /* test for errors */
+     n = select(max_fd, &fdset, NULL, NULL,&tv);
+
      if (n <0){
        printf("Error during select\n");
        continue;
      }
-     n = 0;
-     ///////////////////////////// MAIN LOOP //////////////////////////
-     if(!n){ // no I/O activity, so handle main loop here
-       
+     if(!n)
+     {
        ++tenHzticks;
        if(tenHzticks == 5) {
 	 toggleSpuLed(SPU_LED_RED_ON);  
-	      
        }
 
        if(tenHzticks == 10) {
 	 tenHzticks = 0;
 	 toggleSpuLed(SPU_LED_RED_OFF);  
        }
-
-
        // Poll aggregator to get IMU data
        
        printf("START GPS TRANSACTION**********\n"); 
-       writeCharsToSerialPort(com2, gpsQueryString, strlen(gpsQueryString));
-       usleep(25000);
+
+       //Tell the agg that we want whatever gps data it has to give 
+       writeCharsToSerialPort(com2, AGGR_GPS_QUERY_CMD, strlen(AGGR_GPS_QUERY_CMD));
        readCharsFromSerialPort(com2, buffer, &bytes2,MAX_BUFF_SZ); 
-       /*
-       total_gps_bytes = 0;
-       while(1)  
-       {
-         printf("START GPS READ LOOP\n"); 
-         total_gps_bytes += bytes2;
-         printf("NUM BYTES READ: %d\n",total_gps_bytes); 
-         for(gps_byte_index = 0; gps_byte_index < total_gps_bytes ; gps_byte_index++)   
-         {
-         
-           //printf("READ GPS BYTE: %c\n",buffer[gps_byte_index]); 
-           if(buffer[gps_byte_index] == '\n')
+
+       if(bytes2 > 1) //only handle the gps data if we have it
+       {  
+         if(buffer[bytes2] != AGGR_DATA_XFER_ACK)
+         { 
+           //We didn't get all of the data so we keep reading until we do
+           int total_gps_bytes = 0;
+           total_gps_bytes = bytes2;
+           while(1)  
            {
-             printf("FOUND NEWLINE\n",buffer[gps_byte_index]); 
-             end_gps_sentance = true;   
+             printf("CALLING GPS READ A SECOND TIME\n"); 
+             readCharsFromSerialPort(com2, &buffer[total_gps_bytes], &bytes2,MAX_BUFF_SZ - total_gps_bytes); 
+             total_gps_bytes += bytes2;
+             printf("NUM GPS BYTES READ: %d\n",total_gps_bytes); 
+
+             if(buffer[total_gps_bytes] == AGGR_DATA_XFER_ACK)
+               break; //we got it all so get out of the read loop 
+           }
+            bytes2 = total_gps_bytes; 
+         } 
+         buffer[bytes2+1] = '\0';
+	 if (VERBOSE) printf("\n GPS sentence is \"%s\"\n",buffer);
+      
+         //
+         //we recieved data now parse GPS sentence
+         //
+
+         char* sentptr = NULL;
+         int gpscnt = 0;
+         char gpsBufCpy[MAX_BUFF_SZ + 1];
+         strcpy(gpsBufCpy, buffer);
+         char gpsdelim[1];   
+          gpsdelim[0] = AGGR_MESSAGE_DELIM_END;
+         sentptr = strtok(gpsBufCpy,gpsdelim);
+         while(sentptr != NULL)
+         {
+           switch(gpscnt)
+           {
+             case 0: 
+               status = SWARM_SUCCESS;
+               strcpy(currentOrbLoc.gpsSentence,sentptr);
+               status = parseGPSSentence(&currentOrbLoc);
+               if(status == SWARM_SUCCESS)
+               { 
+        	   if(VERBOSE)
+        	     printf("\n Parsed line %s \n",currentOrbLoc.gpsSentence);
+        	   status = convertNMEAGpsLatLonDataToDecLatLon(&currentOrbLoc);
+        	   if(status == SWARM_SUCCESS)
+        	     {
+        	       if(VERBOSE)
+        		 printf("\n Decimal lat:%lf lon:%lf utctime:%s \n",currentOrbLoc.latdd,currentOrbLoc.londd,currentOrbLoc.nmea_utctime);
+                  
+        	       decimalLatLongtoUTM(WGS84_EQUATORIAL_RADIUS_METERS, WGS84_ECCENTRICITY_SQUARED, &currentOrbLoc);
+                     if(VERBOSE)
+                       printf("Northing:%f,Easting:%f,UTMZone:%s\n",currentOrbLoc.UTMNorthing,currentOrbLoc.UTMEasting,currentOrbLoc.UTMZone);
+        	     }
+               }
+               else
+               {
+        	 if(VERBOSE)
+        	   printf("\n Failed GPS parse status=%i", status);
+               }
+             break;//end GPS Lat/Lon parse
+
+             case 1:   // Parse the gps velocity data 
+               status = SWARM_SUCCESS;
+               strcpy(currentOrbLoc.vtgSentence,sentptr);
+               status = parseGPSSentence(&currentOrbLoc);
+               if(status == SWARM_SUCCESS)
+               { 
+        	 if(VERBOSE)
+        	   printf("\n Parsed line %s \n",currentOrbLoc.vtgSentence);
+               }
+               else
+               {
+	         if (VERBOSE)
+        	   printf("\n Failed VTG gps parse status=%i", status);
+               }
+             break;//end GPS VTG data parse 
+           }
+           gpscnt++; 
+           sentptr = strtok(NULL,SWARM_NMEA_GPS_DATA_DELIM);
+         }
+       }
+       
+       //Tell the agg that we want whatever zigbee data it has to give 
+       writeCharsToSerialPort(com2, AGGR_ZIGBEE_QUERY_CMD, strlen(AGGR_ZIGBEE_QUERY_CMD));
+       readCharsFromSerialPort(com2, buffer, &bytes2,MAX_BUFF_SZ); 
+       if(bytes2 > 1){  //only handle the zigbee data if we have it one byte means only ! 
+         buffer[bytes2+1] = '\0';
+
+	 if (VERBOSE)
+           printf("\n Raw Zigbee data from Aggregator \"%s\"\n",buffer);
+
+         //Now we examine the last character of the read data to see if we have gotten all of the 
+         //data in the first read
+         if(buffer[bytes2] != AGGR_DATA_XFER_ACK)
+         { 
+           //We didn't get all of the data so we keep reading until we do
+           int total_zigbee_bytes = 0;
+           total_zigbee_bytes = bytes2;
+           while(1)  
+           {
+             printf("CALLING ZIGBEE READ A SECOND TIME\n"); 
+             readCharsFromSerialPort(com2, &buffer[total_zigbee_bytes], &bytes2,MAX_BUFF_SZ - total_zigbee_bytes); 
+             total_zigbee_bytes += bytes2;
+             printf("NUM ZIGBEE BYTES READ: %d\n",total_zigbee_bytes); 
+
+             if(buffer[total_zigbee_bytes] == AGGR_DATA_XFER_ACK)
+               break; //we got it all so get out of the read loop 
+           }
+            bytes2 = total_zigbee_bytes; 
+         } 
+         
+         //Now we have all of the data so lets do some work
+
+         //First we have to tokenize the recieved buffer into individual messages
+         char* msgptr = NULL;
+         int msgcount = 0;
+         char zigbeeBufCpy[MAX_BUFF_SZ + 1];
+         int messageType = 0;
+         strcpy(zigbeeBufCpy, buffer);
+         char msgdelim[1];   
+         msgdelim[0] = AGGR_MESSAGE_DELIM_END;
+         msgptr = strtok(zigbeeBufCpy,msgdelim);
+         //fprintf(stderr, "\nstrtok returned **%s** for gps sentence type\n",gpsdata->gpsSentenceType);
+         while(msgptr != NULL)
+         {
+           //do the work 
+           messageType = getMessageType(msgptr);
+           switch(messageType)
+           {
+             case AGGR_MSG_TYPE_MOTHER_SHIP_SPU_POLL: 
+               
+             break;
+             
+             case AGGR_MSG_TYPE_TRAJECTORY: 
+             break;
+
+             case AGGR_MSG_TYPE_MOTHER_SHIP_LOC: 
+             break;
+
+             case AGGR_MSG_TYPE_EFFECTS: 
+               //Write the effects string to com3
+               writeCharsToSerialPort(com3,  msgptr, strlen(msgptr));
+             break;
+
+             case AGGR_MSG_TYPE_MOTOR_CONTROL: 
+               writeCharsToSerialPort(com5,  msgptr, strlen(msgptr));
              break;
            }
+           msgcount++; 
+           msgptr = strtok(NULL,SWARM_NMEA_GPS_DATA_DELIM);
          }
-         if(end_gps_sentance == true)
-         {
-           break;
-         }
-         printf("CALLING READ A SECOND TIME\n"); 
-         readCharsFromSerialPort(com2, &buffer[total_gps_bytes], &bytes2,MAX_BUFF_SZ - total_gps_bytes); 
-       }
-        bytes2 = total_gps_bytes; 
-       */
-
-	 buffer[bytes2+1] = '\0';
-       //printf("END GPS TRANSACTION********** SENT%s\n",buffer); 
-	 if(!bytes2){
-	 if (VERBOSE) printf("\n GPS sentence is \"%s\"\n",buffer);
-	 }
-       //
-       //now parse GPS sentence
-       //
-       swarmGpsData * gpsdata = NULL;
-       int status = SWARM_SUCCESS;
-       gpsdata = (swarmGpsData*) malloc(sizeof(struct swarmGpsData)); 
-       strcpy(gpsdata->gpsSentence,buffer);
-       status = parseGPSSentence(gpsdata);
-       if(status == SWARM_SUCCESS)
-	 { 
-	   if(VERBOSE)
-	     printf("\n Parsed line %s \n",gpsdata->gpsSentence);
-	   status = convertNMEAGpsLatLonDataToDecLatLon(gpsdata);
-	   if(status == SWARM_SUCCESS)
-	     {
-	       if(VERBOSE)
-		 printf("\n Decimal lat:%Lf lon:%Lf utctime:%s \n",gpsdata->latdd,gpsdata->londd,gpsdata->nmea_utctime);
-          
-	       decimalLatLongtoUTM(WGS84_EQUATORIAL_RADIUS_METERS, WGS84_ECCENTRICITY_SQUARED, gpsdata);
-	        if(VERBOSE)
-		  printf("Northing:%f,Easting:%f,UTMZone:%s\n",gpsdata->UTMNorthing,gpsdata->UTMEasting,gpsdata->UTMZone);
-	     }
-        
-	 }
-       else
-	 printf("\n Failed GPS parse status=%i", status);
-       free(gpsdata);
-       //
-       //end GPS parse
        
+       } //End recieved zigbee data if statement
+
        if (VERBOSE){ 
 	 printf("main loop tick %d\n",tenHzticks);
 	 fflush(stdout);
        }
-
-
-
-
      }
-     else { // we have I/O so deal with it 
-       if (FD_ISSET(com2, &input)) // if serial input on COM2
-	 {
-	   
-	   // Right now this is a straight pipe from COM2 to COM5
-	   // so the zigbee can talk to the motor controller directly
-	   //Read data from com2
-	   readCharsFromSerialPort(com2, buffer, &bytes2,MAX_BUFF_SZ); 
-	   buffer[bytes2+1] = '\0';
-	   if(bytes2){
-	     if (VERBOSE) printf("\n Read \"%s\" from  com2\n",buffer);
-	     
-	     //write data to com5
-	     if (VERBOSE) printf("\nWriting back to com5 \n");
-	   }	
-	 }
-       if(FD_ISSET(com4, &input)){ // if serial input on COM4
-	 
-	 // Right now COM4 is a straight pipe back to COM2
-	 // so the GPS can send data back through the zigbee. 
-	 // GPS data is encapsulated with GPS delimiter. 
-	 // when aggregator is aggregating COM4 will be output for LEDs
-	 
-	 //Read data from com4
-	 readCharsFromSerialPort(com4, buffer, &bytes2,MAX_BUFF_SZ); 
-	 buffer[bytes2+1] = '\0';
-	 if (VERBOSE) printf("\n Read the data: \"%s\" from serial port com4\n",buffer);
-	 //FD_CLR(com4,&input);
-	 
-	 //write data to com2
-	 if (VERBOSE) printf("\nWriting \"%s\" data back to com2 \n",buffer);
-	 writeCharsToSerialPort(com2, gps_start_str, strlen(gps_start_str));
-	 writeCharsToSerialPort(com2, buffer, bytes2);
-	 writeCharsToSerialPort(com2, gps_stop_str, strlen(gps_stop_str));
-       }
-       
-       if(FD_ISSET(com5, &input)){
-	 // if input from motor controller (in response to status 
-	 // commands), send it back to the zigbee (aggregator)
-	 
-	 //Read data from com5
-	 readCharsFromSerialPort(com5, buffer, &bytes2,MAX_BUFF_SZ); 
-	 buffer[bytes2+1] = '\0';
-	 if (VERBOSE) printf("\n Read data: \"%s\"  com5\n",buffer);
-	 //FD_CLR(com5,&input);
-	 
-	 if (VERBOSE) printf("\nWriting back to com2 \n");
-	 writeCharsToSerialPort(com2, buffer, bytes2);
-	 
-       }
-     }
-  }
+   }
 } //END main() 
 
     
