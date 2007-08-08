20e32a755aa5e0ac8f5bd369f0acfe0ee717559a
swarm <info@orbswarm.com>
2007-08-08 01:26:56 +0000

    added kalman code
    git-svn-id: http://svn.lee.org/swarm/trunk@251 47a5675e-1c2e-0410-904b-cc838fe3ce0b

---
+++ spu/nmm-kf-kalmanfilter/kalmanswarm.h
@@ -0,0 +1,38 @@
+/*  Function Prototypes for kalmanSwarm */
+
+#define STATE_SIZE         	13
+
+#define RADIUS 0.762
+#define GRAVITY 9.81
+
+/*  These are the components of the state vector   */
+
+#define STATE_vdot             	1
+#define STATE_v                	2
+#define STATE_phidot           	3
+#define STATE_phi              	4
+#define STATE_theta            	5
+#define STATE_psi              	6
+#define STATE_x    		7
+#define STATE_y    		8
+#define STATE_xab    		9
+#define STATE_yab   		10
+#define STATE_zab    		11
+#define STATE_xrb   		12
+#define STATE_zrb   		13
+
+#define MEAS_SIZE         	10
+
+/*  These are the components of the state vector   */
+
+#define MEAS_xa           	1
+#define MEAS_ya                	2
+#define MEAS_za           	3
+#define MEAS_xr              	4
+#define MEAS_zr           	5
+#define MEAS_xg              	6
+#define MEAS_yg    		7
+#define MEAS_psig    		8
+#define MEAS_vg    		9
+#define MEAS_omega   		10
+
