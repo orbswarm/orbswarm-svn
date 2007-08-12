cf2da11d6e38c65eb90f20ce553103b144dc2378
swarm <info@orbswarm.com>
2007-08-12 00:22:37 +0000

    Added a couple of util method signatures
    git-svn-id: http://svn.lee.org/swarm/trunk@287 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- spu/include/swarmGPSutils.h
+++ spu/include/swarmGPSutils.h
@@ -8,10 +8,22 @@
 //
 //      
 //
-//	Written by ????, optional email
+//	Written by Matthew Cline and Jessie, optional email
 // refactored from swarmsputils.c  by Jon Foote (Head Rotor ar rotorbrain.com)
 // -----------------------------------------------------------------------
 
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/mman.h>
+#include<stdio.h>
+#include<fcntl.h>
+#include<string.h>
+#include <sys/stat.h>			/* declare the 'stat' structure	*/
+#include <time.h>
+#include <errno.h>
+#include "swarmdefines.h"
+#include <stdlib.h>
+#include <math.h>
 
 //Parse an NMEA data sentence into the seperate components needed for conversion
 //Returns SWARM_SUCCESS on a successful parse and  SWARM_INVALID_GPS_SENTENCE if 
@@ -21,6 +33,10 @@ int parseGPSSentence(swarmGpsData * gpsdata);
 
 //converts the raw NMEA gps lat long data into decimal lat long data
 int convertNMEAGpsLatLonDataToDecLatLon(swarmGpsData * gpsdata);
+
+int parseGPSVtgSentance(swarmGpsData * gpsdata);
+
+int parseRawAggregatorGPSData(char* rawGPS, swarmGpsData * gpsdata); 
 
 //converts decimal lat/lon to UTM 
 //East Longitudes are positive, West longitudes are negative. 
