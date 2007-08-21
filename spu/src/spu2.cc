ec4b28d3b16ae58b5938c050e005e0148d65d3a5
swarm <info@orbswarm.com>
2007-08-21 07:18:06 +0000

    added Motor Controller verbocity level and io commands
    git-svn-id: http://svn.lee.org/swarm/trunk@404 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- spu/src/spu2.cc
+++ spu/src/spu2.cc
@@ -4,13 +4,20 @@
 #include  <sys/types.h>
 #include  <sys/time.h>
 #include  <sys/select.h>
-#include "../include/swarmserial.h"
 #include "../include/swarmdefines.h"
+#include "../include/swarmserial.h"
 #include "../include/swarmspuutils.h"
 
 
 int main(int argc, char *argv[]) 
 {
+ int ENABLE_DEBUG = 0;
+    fprintf(stderr,"\n  STARTING SPU \n",argc);
+  if(argc >= 2)
+  {
+    fprintf(stderr,"\n ENABLING VERBOSE MESSAGES \n");
+    ENABLE_DEBUG = 1;
+  }
   int com1=0; /* File descriptor for the port */
   int com2=0; /* File descriptor for the port */
   int com3=0, com5=0; 	/* ditto */
@@ -23,14 +30,17 @@ int main(int argc, char *argv[])
   fd_set          input;
   struct timeval tv;
 
+  bool echoMotorControlMsgs = true;
   //Get this orbs Address from its IP address
   char myIP[32];
   getIP("eth0", myIP);
-  fprintf(stderr,"\nMY IP ADDRESS: %s\n",myIP);
+  if(ENABLE_DEBUG)
+    fprintf(stderr,"\nMY IP ADDRESS: %s\n",myIP);
   int myOrbId =0;
   char* orbAddStart = rindex(myIP,'.');
   myOrbId = atoi(&orbAddStart[1]);
-  fprintf(stderr,"\nMY ORB ID: %d\n",myOrbId);
+  if(ENABLE_DEBUG)
+    fprintf(stderr,"\nMY ORB ID: %d\n",myOrbId);
 
 
   com2 = initSerialPort(COM2, 38400);
@@ -85,14 +95,17 @@ int main(int argc, char *argv[])
          if(bytes2 > 0)
          {
            buff2[bytes2] = 0;
-           printf("\n GOT BYTES \"%s\" from  com2\n",buff2);
+           if(ENABLE_DEBUG)
+              printf("\n GOT BYTES \"%s\" from  com2\n",buff2);
+
            while(SWARM_SUCCESS != getMessageForDelims(msgBuff, MAX_BUFF_SZ, &msgSize,
                                    buff2, totalBytes, MSG_HEAD_AGG_STREAM,
                                    MSG_END_AGG_STREAM,true))  
            { 
               if(numTrys >= 10)
               {
-                fprintf(stderr,"\n NUM TRIES EXPIRED AT: %d \n",numTrys);
+                if(ENABLE_DEBUG)
+                  fprintf(stderr,"\n NUM TRIES EXPIRED AT: %d \n",numTrys);
                 break;
               }
               usleep(5000);
@@ -103,7 +116,8 @@ int main(int argc, char *argv[])
 
            bytes2 = totalBytes;
            buff2[bytes2] = 0;
-           printf("\n GOT MESSAGE \"%s\" from  com2\n",msgBuff);
+           if(ENABLE_DEBUG)
+             printf("\n GOT MESSAGE \"%s\" from  com2\n",msgBuff);
            char msgBuff2[MAX_BUFF_SZ + 1];
            int msgSize2 = 0;
            int status = SWARM_SUCCESS;
@@ -114,16 +128,19 @@ int main(int argc, char *argv[])
            {
              strncpy(dummyBuff,&msgBuff[1],3); //extract the orbid max 3 digits 
              msgOrbId = atoi(dummyBuff);  //atoi very forgiving does not complain if whitespace
-            fprintf(stderr, "\nGOT MESSAGE ORB ID : %d\n",msgOrbId);
+            if(ENABLE_DEBUG)
+              fprintf(stderr, "\nGOT MESSAGE ORB ID : %d\n",msgOrbId);
            if(msgOrbId == myOrbId)
            {
-            fprintf(stderr, "\nGOT MESSAGE ORB ID MATCHED\n" );
+            if(ENABLE_DEBUG)
+              fprintf(stderr, "\nGOT MESSAGE ORB ID MATCHED\n" );
            if(SWARM_SUCCESS == getMessageForDelims(msgBuff2, MAX_BUFF_SZ, &msgSize2,
                                    msgBuff, msgSize, MSG_HEAD_MOTOR_CONTROLER,
                                    MSG_END_MOTOR_CONTROLER,true))  
            {
               //write data to com5
-              printf("\nWriting back to com5 \n");
+              if(ENABLE_DEBUG)
+                printf("\nWriting back to com5 \n");
               writeCharsToSerialPort(com5, msgBuff2, msgSize2);
            }
            if(SWARM_SUCCESS == getMessageForDelims(msgBuff2, MAX_BUFF_SZ, &msgSize2,
@@ -131,18 +148,35 @@ int main(int argc, char *argv[])
                                                         MSG_END_LIGHTING ,true))  
            {
               //write data to com3
-                printf("\nWriting lighting data back to com3 \n");
+                if(ENABLE_DEBUG)
+                  printf("\nWriting lighting data back to com3 \n");
                 writeCharsToSerialPort(com3, msgBuff2, msgSize2);
            } 
            if(SWARM_SUCCESS == getMessageForDelims(msgBuff2, MAX_BUFF_SZ, &msgSize2,
                                                         msgBuff, msgSize, MSG_HEAD_MOTHER_SHIP,
                                                         MSG_END_MOTHER_SHIP ,false))  
            {
-                fprintf(stderr,"\n GOT MSHIP CMD ***%s*** \n",msgBuff2);
+                if(ENABLE_DEBUG)
+                  fprintf(stderr,"\n GOT MSHIP CMD ***%s*** \n",msgBuff2);
                 if(strcmp(msgBuff2,MOTHER_SHIP_MSG_HEAD_STATUS) == 0)
                 {
-                  fprintf(stderr, "\nIDENTIFIED MESSAGE POLL MESSAGE\n");
+                  if(ENABLE_DEBUG)
+                    fprintf(stderr, "\nIDENTIFIED MESSAGE POLL MESSAGE\n");
                   writeCharsToSerialPort(com2, msgBuff2,msgSize2);
+                }          
+                else if(strcmp(msgBuff2,MOTHER_SHIP_MSG_LOW_VERBOCITY) == 0)
+                {
+               
+                  if(ENABLE_DEBUG)
+                    fprintf(stderr, "\nIDENTIFIED LOW VERBOCITY MESSAGE\n");
+                  echoMotorControlMsgs  = false;
+                }          
+                else if(strcmp(msgBuff2,MOTHER_SHIP_MSG_HIGH_VERBOCITY) == 0)
+                {
+                  if(ENABLE_DEBUG)
+                    fprintf(stderr, "\nIDENTIFIED HIGH VERBOCITY MESSAGE\n");
+                  flushSerialPort(com5);
+                  echoMotorControlMsgs  = true;
                 }          
               /* 
                 else if(strcmp(dummyBuff,MOTHER_SHIP_MSG_HEAD_TRAJECTORY) == 0)
@@ -171,18 +205,22 @@ int main(int argc, char *argv[])
            //printf("\n Read \"%s\" from  com2\n",buff2);
         }
               
-
-        bytes2 = 0; 
-          //Read data from com5
-        readCharsFromSerialPort(com5, buff2, &bytes2,MAX_BUFF_SZ); 
-        buff2[bytes2] = '\0';
-        if(bytes2 > 0)
-        {
-          printf("\n Read data: \"%s\"  com5\n",buff2);
-          writeCharsToSerialPort(com2, buff2,bytes2);
+        if(echoMotorControlMsgs)
+        { 
+          bytes2 = 0; 
+            //Read data from com5
+          readCharsFromSerialPort(com5, buff2, &bytes2,MAX_BUFF_SZ); 
+          buff2[bytes2] = '\0';
+          if(bytes2 > 0)
+          {
+            if(ENABLE_DEBUG) 
+              printf("\n Read data: \"%s\"  com5\n",buff2);
+            writeCharsToSerialPort(com2, buff2,bytes2);
+          }
         }
       }
-	 printf("main loop tick %d\n",tenHzticks);
+         if(ENABLE_DEBUG) 
+	   printf("main loop tick %d\n",tenHzticks);
   }
 } //END main() 
 
--- spu/src/swarmserial.cc
+++ spu/src/swarmserial.cc
@@ -24,7 +24,7 @@ int initSerialPort(const char* port, int baud)
   /*
    * Get the current options for the port...
    */
-  fprintf(stderr,"\n Getting current options\n");
+  //fprintf(stderr,"\n Getting current options\n");
   tcgetattr(fd, &options);
   /*
    * Set the baud rates to 115200...
@@ -48,7 +48,7 @@ int initSerialPort(const char* port, int baud)
       fprintf(stderr,"\n unsupported bit rate %d\n",baud);
     }
 
-  fprintf(stderr,"\n Setting Baud rate to %d\n",baud);
+  //fprintf(stderr,"\n Setting Baud rate to %d\n",baud);
     cfsetispeed(&options, brate);
     cfsetospeed(&options, brate);
 
@@ -64,7 +64,7 @@ int initSerialPort(const char* port, int baud)
     /*
    * Setup port in raw data mode.
    */
-  fprintf(stderr,"\n Setting port to raw data mode\n");
+  //fprintf(stderr,"\n Setting port to raw data mode\n");
   options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
   /*
    * Enable the receiver and set local mode...
@@ -79,80 +79,9 @@ int initSerialPort(const char* port, int baud)
   /*
    * Set the new options for the port...
    */
-  fprintf(stderr,"\n Setting Config options to port\n");
+  //fprintf(stderr,"\n Setting Config options to port\n");
   tcsetattr(fd, TCSANOW, &options);
-  fprintf(stderr,"\n Done Setting Config options to port\n");
-
-  return fd;
-}
-/* this is the OLD version -- had some undebuggable problems */
-int OLDinitSerialPort(const char* port, int baud)
-{
-  int fd; /* File descriptor for the port */
-  struct termios options;
-  speed_t brate = baud; /* let you override switch below if needed */
-
-  fd = open(port, O_RDWR | O_NOCTTY | O_NDELAY);
-  if(fd == -1){
-    fprintf(stderr,"failed to open serial port%s\n",port);
-  }
-
-  /*
-   * Get the current options for the port...
-   */
-  fprintf(stderr,"\n Getting current options\n");
-  tcgetattr(fd, &options);
-  /*
-   * Set the baud rates to 115200...
-   */
-  switch(baud) 
-    {
-    case 4800:   brate=B4800;   break;
-    case 9600:   brate=B9600;   break;
-      // if you want these speeds, uncomment these and set #defines if Linux
-      //#ifndef OSNAME_LINUX
-      //    case 14400:  brate=B14400;  break;
-      //#endif
-    case 19200:  brate=B19200;  break;
-      //#ifndef OSNAME_LINUX
-      //    case 28800:  brate=B28800;  break;
-      //#endif
-    case 38400:  brate=B38400;  break;
-    case 57600:  brate=B57600;  break;
-    case 115200: brate=B115200; break;
-    default:	 
-      fprintf(stderr,"\n unsupported bit rate %d\n",baud);
-    }
-
-  fprintf(stderr,"\n Setting Baud rate to %d\n",baud);
-    cfsetispeed(&options, brate);
-    cfsetospeed(&options, brate);
-
-  /*
-   * Set port to 8N1 pairity
-   */
-  options.c_cflag &= ~PARENB;
-  options.c_cflag &= ~CSTOPB;
-  options.c_cflag &= ~CSIZE;
-  options.c_cflag |= CS8;
-
-  /*
-   * Setup port in raw data mode.
-   */
-  fprintf(stderr,"\n Setting port to raw data mode\n");
-  options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-
-  /*
-   * Enable the receiver and set local mode...
-   */
-  options.c_cflag |= (CLOCAL | CREAD);
-
-  /*
-   * Set the new options for the port...
-   */
-  fprintf(stderr,"\n Setting Config options to port\n");
-  tcsetattr(fd, TCSANOW, &options);
-  fprintf(stderr,"\n Done Setting Config options to port\n");
+  //fprintf(stderr,"\n Done Setting Config options to port\n");
 
   return fd;
 }
@@ -184,6 +113,12 @@ int writeCharsToSerialPort(int port_fd, char* buff,
    //tcflush(port_fd, TCOFLUSH);
    //tcdrain(port_fd);
    return status;
+}
+
+void flushSerialPort(int port_fd)
+{
+   tcflush(port_fd, TCIFLUSH);
+   tcdrain(port_fd);
 }
 
 //Reads data from serial port, port_fd, until either the ack character is 
