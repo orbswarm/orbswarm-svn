db68cd9b00b9e64e7ddac08ad437b223a0a9372c
swarm <info@orbswarm.com>
2007-12-08 04:57:13 +0000

    code clean-up pt I
    git-svn-id: http://svn.lee.org/swarm/trunk@553 47a5675e-1c2e-0410-904b-cc838fe3ce0b

---
+++ aggr/src/blinky_test.c
@@ -0,0 +1,17 @@
+#include "include/timer0.h"
+
+int main(void)
+{
+
+  DDRB=0xff;
+  //PORTJ=0xff;
+  //Direction bit = PC1
+  //PORTB = PORTB | (1<<PB5);
+  //PORTJ  = PORTJ | (1<<PJ3);
+   PORTB = PORTB ^ (1<<PB6);
+   while(1){
+    loopTimer0(1000);
+    PORTB = PORTB ^ (1<<PB6);
+  }
+
+}
