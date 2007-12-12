1f3c3baf96425bd0772c9be8f2c7649f9cd40776
swarm <info@orbswarm.com>
2007-12-12 09:41:16 +0000

    random stuff
    git-svn-id: http://svn.lee.org/swarm/trunk@570 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- aggr/src/swarm_queues.c
+++ aggr/src/swarm_queues.c
@@ -15,14 +15,14 @@ volatile static struct SWARM_MSG s_queue[MAX_SWARM_MSG_BUS_SIZE];
 volatile static unsigned long s_nHeadIdx=0;
 volatile static unsigned long s_nTailIdx=0;
 
-static void (* _debug)(char*) =0;
+static void (* _debug)(const char*) =0;
 
-void initSwarmMsgBus(void (*debug)(const char *c))
+void initSwarmMsgBus(void (*debug)(const char* c))
 {
 	_debug=debug;
 }
 
-static void DEBUG(char* s)
+static void DEBUG(const char* s)
 {
 	if(_debug != 0)
 		(*_debug)(s);
@@ -80,16 +80,16 @@ void pushXbeeDataQ(const char* msg)
 
 }
 
-static volatile int s_nSwarmMsgBusPopLock=0;
+static volatile int s_nSwarmMsgBusLock=0;
 
 void pushSwarmMsgBus(struct SWARM_MSG msg, int isInterruptCtx)
 {
   unsigned long nTmpHead;
   while(1){
-  	if(s_nSwarmMsgBusPopLock == 0)
+  	if(s_nSwarmMsgBusLock == 0)
   	{
-  	  	s_nSwarmMsgBusPopLock++;
-  		if(1==s_nSwarmMsgBusPopLock)
+  	  	s_nSwarmMsgBusLock++;
+  		if(1==s_nSwarmMsgBusLock)
 	  	{
 		  nTmpHead = ( s_nHeadIdx + 1) & SWARM_MSG_BUS_MASK;
 		  while(nTmpHead == s_nTailIdx){
@@ -99,10 +99,10 @@ void pushSwarmMsgBus(struct SWARM_MSG msg, int isInterruptCtx)
 		  }
 		  s_queue[nTmpHead]=msg;
 		  s_nHeadIdx=nTmpHead;
-		  s_nSwarmMsgBusPopLock--;
+		  s_nSwarmMsgBusLock--;
 		  return;
 	  	}
-	  	s_nSwarmMsgBusPopLock--;
+	  	s_nSwarmMsgBusLock--;
 	  	return;
   	}
   	else if(isInterruptCtx)
@@ -120,29 +120,29 @@ struct SWARM_MSG popSwarmMsgBus(int isInterruptCtx)
   strcpy(msg.swarm_msg_payload, "Null msg");
   while(1)
   {
-  	  if(s_nSwarmMsgBusPopLock == 0)
+  	  if(s_nSwarmMsgBusLock == 0)
   	  {
-  	  	sprintf(debugMsg, "lock is 0 s_nSwarmMsgBusPopLock=%d",s_nSwarmMsgBusPopLock);
+  	  	sprintf(debugMsg, "lock is 0 s_nSwarmMsgBusPopLock=%d",s_nSwarmMsgBusLock);
   	  	DEBUG(debugMsg);
-  	  	s_nSwarmMsgBusPopLock++;
-  		if(1==s_nSwarmMsgBusPopLock)
+  	  	s_nSwarmMsgBusLock++;
+  		if(1==s_nSwarmMsgBusLock)
   		{ 
 		  unsigned long nTmpTail;
 		  if(s_nHeadIdx == s_nTailIdx){
-		  	s_nSwarmMsgBusPopLock--;
+		  	s_nSwarmMsgBusLock--;
 		    return msg;
 		  }
 		  nTmpTail= (s_nTailIdx + 1) & SWARM_MSG_BUS_MASK;
 		  s_nTailIdx=nTmpTail;
 		  struct SWARM_MSG tempMsg =s_queue[nTmpTail]; 
-		  s_nSwarmMsgBusPopLock--;
+		  s_nSwarmMsgBusLock--;
 		  return tempMsg;
   		}
-  		s_nSwarmMsgBusPopLock--;
+  		s_nSwarmMsgBusLock--;
   	  }
   	  else
   	  {
-  	  	sprintf(debugMsg, "lock is not 0 s_nSwarmMsgBusPopLock=%d",s_nSwarmMsgBusPopLock);
+  	  	sprintf(debugMsg, "lock is not 0 s_nSwarmMsgBusPopLock=%d",s_nSwarmMsgBusLock);
   	  	DEBUG(debugMsg);
   	  	if(isInterruptCtx)
   	  		return msg;
