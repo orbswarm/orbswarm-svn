04092d24dcc0669458e143f9931315b7519f4b93
swarm <info@orbswarm.com>
2008-03-31 05:37:38 +0000

    creating branch for mezzanine level functionality
    git-svn-id: http://svn.lee.org/swarm/trunk@653 47a5675e-1c2e-0410-904b-cc838fe3ce0b

---
+++ spu/dispatcher_mezzanine/kalman.h
@@ -0,0 +1,36 @@
+/*  kalman.h
+    This is the include file for the kalman filtering functions defined
+    in kalman.c.
+
+    This code comes from a mixture of sources, and at this time, may not have clear rights.
+    Redistribute at your own risk.
+
+*/
+
+#undef PRINT_DEBUG
+
+#include "matmath.h"    /*  uFloat decl. and matrix utilities */
+
+
+
+/***********   Extended Kalman Filtering   **************/
+
+extern void extended_kalman_init( uFloat **P, uFloat *x );
+
+extern void extended_kalman_step( uFloat *z_in );
+
+extern uFloat *kalman_get_state( void );  
+
+
+/*********    Application Specific Functions  ***********/
+
+extern void generate_measurement_transfer( uFloat *state, uFloat **H );
+
+extern void apply_measurement( uFloat *newState, uFloat *est_measurement );
+extern void systemF( uFloat *stateDot, uFloat *state );
+extern void systemJacobian( uFloat *state, uFloat **jacobian );
+
+extern void covarianceSet( uFloat **GQGt, uFloat **R );
+
+/*  and a select set of global variables to communicate with them.  */
+
---
+++ spu/dispatcher_mezzanine/swarmdefines.h
@@ -0,0 +1,193 @@
+/******Swarm Defines include file *********/
+#ifndef SWARM_DEFINES_H
+#define SWARM_DEFINES_H 1
+
+#define VERBOSE 1 //set to 1 for increased debugging messages
+
+#define MAX_BUFF_SZ 1024 //Serial port read/write buffer size
+#define SWARM_SUCCESS 0
+#define SWARM_SERIAL_WRITE_ERR 10 //failed to write data to serial port 
+#define SWARM_OUT_OF_MEMORY_ERROR 11 //failed to malloc some memory  
+
+#define MAX_LOG_ENTRY_SZ 1024 
+#define MAX_NUM_LOG_FILES 5      //the maximum number of log files allowed
+#define MAX_LOG_FILE_SZ 262144
+
+#define MAX_LOG_FILE_NAME_SZ 512 
+
+#define LOG_FILE_BASE_NAME "swarm_log_"
+#define DEFAULT_LOG_PATH "./logs"
+
+//Constants for converting from lat/lon to UTM
+#define WGS84_EQUATORIAL_RADIUS_METERS 6387137
+#define WGS84_ECCENTRICITY_SQUARED 0.00669438
+
+// defines for use with the AD Converters
+#define SONAR_CHANNEL 2
+#define SONAR_MAX_VOLTAGE 5.0
+#define SONAR_SAMPLE_PRECISION 2
+
+#define AD_DEFAULT_MAX_VOLTAGE 5.0 // the default max voltage that will be expected on the AD converter input pins. For differing voltages, we should put specialized code in getAdConverterStatus() in adconverter.cc
+                                   // (for instance, on the line monitoring the batteries)
+#define AD_DEFAULT_PRECISION 2     // when reading multiple values from the AD converter, not pausing between reads produces errors. a value of 1 will not pause, anything greater than 1 will pause
+
+#define BATTERY_CHANNEL 3
+#define BATTERY_MAX_VOLTAGE 26
+
+/************************Constants**************************************/
+/*
+const double PI = 3.14159265;
+const double FOURTHPI = PI / 4;
+const double deg2rad = PI / 180;
+const double rad2deg = 180.0 / PI;
+*/
+/*************************STRUCTURES************************************/
+typedef struct	swarmGpsDataStruct
+{
+  char gpsSentenceType[32]; 
+  char gpsSentence[MAX_GPS_SENTENCE_SZ];
+  char vtgSentence[MAX_GPS_SENTENCE_SZ];
+  char nmea_utctime[64];
+  double nmea_latddmm;
+  double nmea_londdmm;
+  char nmea_latsector;
+  char nmea_lonsector;
+  double latdd;
+  double londd;
+  double utcTime;
+  double metFromMshipNorth;
+  double metFromMshipEast;
+  double utcTimeMship;
+  double UTMNorthing;
+  double UTMEasting;
+  char UTMZone[32];
+  float nmea_course; //heading radians respect to due east
+  float speed; //meters per second
+  char mode;
+} swarmGpsData;
+
+struct	swarmImuData
+{
+  /* IMU data converted to SI units */
+  double si_ratex;
+  double si_ratey;
+  double si_accx;
+  double si_accy;
+  double si_accz;
+
+  /* raw data from IMU */
+  int int_ratex;
+  int int_ratey;
+  int int_accx;
+  int int_accy;
+  int int_accz;
+  int int_vref; 	     /* this is vref on the IMU. Should not change */
+  int int_adc0;		     /* this is the steering pot raw value */
+  int int_adc1;		     /* this is the current sensor raw value */
+
+
+  /* integer bias (what we get when we measure a zero reading */
+  /* subtract these values from the raw int vals to get a zero-mean value*/
+  /* [even though these are doubles, their units are still raw (NOT SI!)] */
+  double ratex_bias;
+  double ratey_bias;
+  double accx_bias;
+  double accy_bias;
+  double accz_bias;
+  double vref_bias;
+  double adc0_bias;
+  double adc1_bias;
+
+  /* these hold the descriptive strings scanned by the parser */
+  /* useful for debug */
+  char adc0_str[10];
+  char adc1_str[10];
+  char ratex_str[10];
+  char ratey_str[10];
+  char accx_str[10];
+  char accy_str[10];
+  char accz_str[10];
+  char vref_str[10];
+};
+
+// store motor data; I think we only care about current & odometer
+// but get them anyway
+struct	swarmMotorData
+{
+
+  /* steering motor data */
+  int steerTarget;		/* target steering angle for PID */
+  int steerActual; 		/* measured steering angle (arbitrary units) */
+  int steerPWM;			/* what we're sending to the steering motor */
+
+  /* drive motor data */
+  int driveTarget;		/* target speed for PID loop */
+  int driveActual;		/* actual measured speed */
+  int drivePWM;			/* what we are sending to the motor control */
+  int odometer;		/* odometer count (will roll over) */
+  int rawCurrent;		/* raw current measure */
+  double speedRPS;		/* speed in radians per seconds */
+
+  /* strings for debug */
+  char steerTarget_str[10];
+  char steerActual_str[10];
+  char steerPWM_str[10];
+
+  char driveTarget_str[10];
+  char driveActual_str[10];
+  char drivePWM_str[10];
+  char odometer_str[10];
+  char rawCurrent_str[10];
+
+};
+
+struct	swarmAdcData
+{
+  /* IMU data converted to SI units */
+  double adcCH0;
+  double adcCH1;
+  double adcCH2;
+  double adcCH3;
+  double adcCH4;
+};
+
+struct  swarmStateEstimate
+{
+  double vdot;
+  double v;
+  double phidot;
+  double phi;
+  double psi;
+  double theta;
+  double x;
+  double y;
+};
+
+struct swarmFeedback
+{
+  double vDes;
+  double deltaDes;
+};
+
+struct swarmPID
+{
+  double error;
+  double lastError;
+  double deadBand;
+  double Kp;
+  double Ki;
+  double Kd;
+  double minDrive;
+  double maxDrive;
+  double iSum;
+  double iLimit;
+  double debugOutput; // flag for outputing PID tuning info
+};
+
+struct spuADConverterStatus {
+	double ad_vals[5];
+	double sonar; // this is in inches
+	double battery_voltage;
+};
+
+#endif
