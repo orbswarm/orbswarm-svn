5324000c56f132bd45ee734afbe75fc1fcc66216
Michael Prados <mprados@gmail.com>
2008-07-24 08:00:45 +0000

    updated comments in pathfollow
    git-svn-id: http://svn.lee.org/swarm/trunk@800 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- spu/dispatcher/pathfollow.h
+++ spu/dispatcher/pathfollow.h
@@ -11,27 +11,46 @@
 
 struct	swarmCoord
 {
+  // an x,y coordinate and some related attributes
 
-  /* steering motor data */
-  double x;	    
-  double y;
-  double psi;
-  double v;
+  double x;		// x position (meters East)	    		
+  double y;		// y position (meters North)
+  double psi;		// heading (radians counterclockwise from East)
+  double v;		// velocity 	
 };
 
 struct swarmCircle
 {
-  struct swarmCoord center;
-  struct swarmCoord current;
-  double radius;
-  double direction;
-  double carrotDistance;
+  struct swarmCoord center;	// location of circle center
+  struct swarmCoord current;	// current position along circle circumference
+  double radius;		
+  double direction;		// right hand rule: +1 for counterclockwise, -1 for clockwise
+  double carrotDistance;	// length to place carrot in front of current
 };
 
+// ------------------------------------------------------------------------
+// headingError
+//
+// given current state and desired carrot coordinates,
+// returns heading error to carrot.
+// ------------------------------------------------------------------------
 double headingError(struct swarmStateEstimate * stateEstimate, struct swarmCoord * carrot);
 
+// ------------------------------------------------------------------------
+// circleInit
+// 
+// Initializes a circle by finding the circle center based on current position
+// and heading.
+//
+// ------------------------------------------------------------------------
 void circleInit( struct swarmStateEstimate * stateEstimate, struct swarmCircle * circle );
 
+// ------------------------------------------------------------------------
+// circlePath
+// 
+// Finds current location on the circular path,
+// and figures out the carrot position
+// ------------------------------------------------------------------------
 void circlePath( struct swarmCircle * circle, struct swarmStateEstimate * stateEstimate, 
 		 struct swarmCoord * carrot );
 
