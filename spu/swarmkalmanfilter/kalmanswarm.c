656ff7e10a86487678db2dafacb2e899faa737ac
Michael Prados <mprados@gmail.com>
2008-08-01 03:29:43 +0000

    git-svn-id: http://svn.lee.org/swarm/trunk@837 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- spu/swarmkalmanfilter/kalmanswarm.c
+++ spu/swarmkalmanfilter/kalmanswarm.c
@@ -131,11 +131,11 @@ void zeroStateEstimates( struct swarmStateEstimate * stateEstimate )
   stateEstimate->theta 	= 0.0;  // frontward pitch
   stateEstimate->x 		= 0;  // meters East
   stateEstimate->y 		= 0;	// meters North
-  stateEstimate->xab	= 0.86;	// x accelerometer bias
-  stateEstimate->yab	= 1.52;	// y accelerometer bias
-  stateEstimate->zab	= -1.87;	// z accelerometer bias
-  stateEstimate->xrb	= -0.26;	// x rate gyro bias
-  stateEstimate->zrb	= 2.316;	// z rate gyro bias
+  stateEstimate->xab	= 0.825;	// x accelerometer bias
+  stateEstimate->yab	= 0.402;	// y accelerometer bias
+  stateEstimate->zab	= -1.945;	// z accelerometer bias
+  stateEstimate->xrb	= -0.135;	// x rate gyro bias
+  stateEstimate->zrb	= 2.282;	// z rate gyro bias
 }
 
 
@@ -147,7 +147,7 @@ void systemF( uFloat *stateDot, uFloat *state )
 {
 	stateDot[ STATE_vdot ] 		= 0.0;
 	stateDot[ STATE_v ] 		= state[ STATE_vdot ];
-	stateDot[ STATE_phidot ] 	= 0.0;
+	stateDot[ STATE_phidot ] 	= - state[ STATE_phi ] * 0.5;
 	stateDot[ STATE_phi ] 		= state[ STATE_phidot ];
 	stateDot[ STATE_theta ] 	= -state[ STATE_theta ] * 1.0;
 	stateDot[ STATE_psi ] 		= -state[ STATE_v ] * tan( state[ STATE_phi ] ) / RADIUS;
@@ -205,7 +205,7 @@ void systemJacobian( uFloat *state, uFloat **jacobian )
 	jacobian[ STATE_phidot ][ STATE_vdot ] 	= 0.0;
 	jacobian[ STATE_phidot ][ STATE_v ] 	= 0.0;
 	jacobian[ STATE_phidot ][ STATE_phidot ]= 0.0;
-	jacobian[ STATE_phidot ][ STATE_phi ] 	= 0.0;
+	jacobian[ STATE_phidot ][ STATE_phi ] 	= -0.5;
 	jacobian[ STATE_phidot ][ STATE_theta ] = 0.0;
 	jacobian[ STATE_phidot ][ STATE_psi ] 	= 0.0;
 	jacobian[ STATE_phidot ][ STATE_x ] 	= 0.0;
@@ -574,19 +574,19 @@ void covarianceSet( uFloat **Qk, uFloat **R )
 
   /* These values actually define Qc, then we multiply by PERIOD to get Qk */
 
-  Qk[ STATE_vdot ][ STATE_vdot ] 	= 0.00001;
+  Qk[ STATE_vdot ][ STATE_vdot ] 	= 0.0001;
   Qk[ STATE_v ][ STATE_v ] 			= 0.0;
-  Qk[ STATE_phidot ][ STATE_phidot ]= 0.0000001;
+  Qk[ STATE_phidot ][ STATE_phidot ]= 0.00001;
   Qk[ STATE_phi ][ STATE_phi ] 		= 0.0;
-  Qk[ STATE_theta ][ STATE_theta ] 	= 0.001;
-  Qk[ STATE_psi ][ STATE_psi ] 		= 0.00005;
+  Qk[ STATE_theta ][ STATE_theta ] 	= 0.0002;
+  Qk[ STATE_psi ][ STATE_psi ] 		= 0.000005;
   Qk[ STATE_x ][ STATE_x ] 			= 0.0;
   Qk[ STATE_y ][ STATE_y ] 			= 0.0;
-  Qk[ STATE_xab ][ STATE_xab ] 		= 0.000001;
-  Qk[ STATE_yab ][ STATE_yab ] 		= 0.000001;
-  Qk[ STATE_zab ][ STATE_zab ] 		= 0.000001;
-  Qk[ STATE_xrb ][ STATE_xrb ] 		= 0.0000005;
-  Qk[ STATE_zrb ][ STATE_zrb ]  	= 0.0000005;
+  Qk[ STATE_xab ][ STATE_xab ] 		= 0.000005;
+  Qk[ STATE_yab ][ STATE_yab ] 		= 0.0001;
+  Qk[ STATE_zab ][ STATE_zab ] 		= 0.000005;
+  Qk[ STATE_xrb ][ STATE_xrb ] 		= 0.000005;
+  Qk[ STATE_zrb ][ STATE_zrb ]  	= 0.000005;
 
   matMultScalar( Qk, PERIOD, Qk, STATE_SIZE, STATE_SIZE );
 
@@ -594,15 +594,15 @@ void covarianceSet( uFloat **Qk, uFloat **R )
     for( col = 1; col <= MEAS_SIZE; col++ )
       R[ row ][ col ] = 0.0;
 
-  R[ MEAS_xa ][ MEAS_xa ] 		= 0.5;
-  R[ MEAS_ya ][ MEAS_ya ] 		= 0.05;
-  R[ MEAS_za ][ MEAS_za ] 		= 0.5;
-  R[ MEAS_xr ][ MEAS_xr ] 		= 0.5;
-  R[ MEAS_zr ][ MEAS_zr ] 		= 0.01;
+  R[ MEAS_xa ][ MEAS_xa ] 		= 0.3;
+  R[ MEAS_ya ][ MEAS_ya ] 		= 0.3;
+  R[ MEAS_za ][ MEAS_za ] 		= 0.3;
+  R[ MEAS_xr ][ MEAS_xr ] 		= 0.1;
+  R[ MEAS_zr ][ MEAS_zr ] 		= 0.1;
   R[ MEAS_xg ][ MEAS_xg ] 		= 5.0;
   R[ MEAS_yg ][ MEAS_yg ] 		= 5.0;
-  R[ MEAS_psig ][ MEAS_psig ] 	= 2*PI;
-  R[ MEAS_vg ][ MEAS_vg ] 		= 2.0;
-  R[ MEAS_omega ][ MEAS_omega ] = 0.01;
+  R[ MEAS_psig ][ MEAS_psig ] 	= 4*PI;
+  R[ MEAS_vg ][ MEAS_vg ] 		= 5.0;
+  R[ MEAS_omega ][ MEAS_omega ] = 0.1;
 }
 
