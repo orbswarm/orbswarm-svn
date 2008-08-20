272a03210051599cd0f99bbd02022f6802887442
Jonathan Foote <jtfoote@ieee.org>
2008-08-20 09:29:13 +0000

    git-svn-id: http://svn.lee.org/swarm/trunk@938 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- spu/dispatcher/spumon.sh
+++ spu/dispatcher/spumon.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
 
-cd /root
-while true
-do 
-    dispatcher_pids=`pidof dispatcher`
-    echo "dispatcher pids=${dispatcher_pids}"
-    pid_cnt=0
-    for dispatcher_pid in ${dispatcher_pids}
-    do
-	pid_cnt=$[${pid_cnt} + 1]
-    done
-    echo "pid_cnt=${pid_cnt}"
-    if [ ${pid_cnt} != 3 ]; then
-	pkill dispatcher
-	echo "killed old dispatcher"
-	nohup ./dispatcher >stdout.log 2>&1 &
-	echo "restarted dispatcher"
-    else
-	echo "dispatcher is running"
-    fi
-    sleep 5 
-done
+exe=joypatcher
+opts=
+while [ 1 ]
+do
+  pidof $exe > /dev/null
+  if [ $? -eq 0 ]
+  then
+    echo "running" > /dev/null
+  else
+    echo "not" > /dev/null
+    /root/$exe > /dev/null
+  fi
+  sleep 1
+  done
+exit 0
