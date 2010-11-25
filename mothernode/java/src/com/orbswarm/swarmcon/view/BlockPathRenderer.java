b6553b0ddf0146eec8e19918c094b38037b55bda
Robert Harris <trebor@trebor.org>
2010-11-25 01:30:08 +0000

    a little log cleanup
    git-svn-id: http://svn.lee.org/swarm/trunk@1092 47a5675e-1c2e-0410-904b-cc838fe3ce0b

--- mothernode/java/src/com/orbswarm/swarmcon/view/BlockPathRenderer.java
+++ mothernode/java/src/com/orbswarm/swarmcon/view/BlockPathRenderer.java
@@ -45,25 +45,25 @@ public class BlockPathRenderer extends ARenderer<IBlockPath>
   {
     g.transform(bp.getTransform());
 
-    if (bp.getBlocks().size() > 0)
-    {
-      g.setColor(new Color(0, 0, 0, 128));
-      g.setStroke(new BasicStroke(0.05f));
-      PathTool pt = new PathTool(bp.getPath(), 0);
-      double step = pt.getLength() / (pt.getLength() / .5);
-      Collection<PathTool.PathPoint> points =
-        new Vector<PathTool.PathPoint>();
-      points.add(pt.getStartPoint());
-      for (double d = 0; d < pt.getLength(); d += step)
-        points.add(pt.getPathPoint(d));
-      points.add(pt.getEndPoint());
-      for (PathTool.PathPoint pp : points)
-      {
-        Point2D ep = pp.getAngle().cartesian(1, pp);
-        Line2D line = new Line2D.Double(pp, ep);
-        g.draw(line);
-      }
-    }
+//    if (bp.getBlocks().size() > 0)
+//    {
+//      g.setColor(new Color(0, 0, 0, 128));
+//      g.setStroke(new BasicStroke(0.05f));
+//      PathTool pt = new PathTool(bp.getPath(), 0);
+//      double step = pt.getLength() / (pt.getLength() / .5);
+//      Collection<PathTool.PathPoint> points =
+//        new Vector<PathTool.PathPoint>();
+//      points.add(pt.getStartPoint());
+//      for (double d = 0; d < pt.getLength(); d += step)
+//        points.add(pt.getPathPoint(d));
+//      points.add(pt.getEndPoint());
+//      for (PathTool.PathPoint pp : points)
+//      {
+//        Point2D ep = pp.getAngle().cartesian(1, pp);
+//        Line2D line = new Line2D.Double(pp, ep);
+//        g.draw(line);
+//      }
+//    }
 
     if (bp.isSelected())
     {
